9986475f7889eb7d714eeebc07dd1015f4ecd645
welshej <welshej@gmail.com>
2014-09-16 11:57:34 -0400

    Fixed Command Line Arguments Off-By-One Error

--- Socket/Socket/main.c
+++ Socket/Socket/main.c
@@ -1,3 +1,7 @@
+#import <stdlib.h>
+#import <stdio.h>
+#import <string.h>
+
 /*
 	Joshua Miner and Ethan Welsh
 	CS1652 Fall 2014
@@ -5,26 +9,49 @@
 	HTTP Client
 */
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) // http_client k|u server_name server_port server_path
 {
-    if(argc!=5)	// If there are not 5 arguments on the command line
+    printf("There are %d args\n", argc);
+    printf("ARG 1: %c\n", atoi(argv[2]));
+
+
+    if(argc != 6)	// If there are not 5 arguments on the command line
     {
         printf("Improperly formatted command line. Try again \n");		// Print what the error is
         printf("Format is http_client k|u server_name server_port server_path\n");		// Format
         return EXIT_FAILURE;
     }
-    if(argv[1]=='k')	// Operating in the Kernel Stack
-    {
-        return 0;	// All operations were successful
-    }
-    else if(argv[1]=='u')
-    {
-        // Operating in the User (Minet) Stack
-        // We don't have to build this one for the project?
-        return 0;	// Operations were successful
-    }
     else
     {
-        return -1;		// There was an issue encountered
+        int http_client = atoi(argv[1]);
+        char* mode = argv[2];
+        char* server_name = argv[3];
+        int server_port = atoi(argv[4]);
+        char* server_path = argv[5];
+
+        printf("CLIENT: %d\n", http_client);
+        printf("MODE: %s\n", mode);
+        printf("SERVER NAME: %s\n", server_name);
+        printf("SERVER PORT: %d\n", server_port);
+        printf("SERVER PATH: %s\n", server_path);
+
+        if(strncmp("k", mode, 1) == 0)	// Operating in the Kernel Stack
+        {
+            printf("K MODE\n");
+            return 0;	// All operations were successful
+        }
+        else if(strncmp("u", mode, 1) == 0)
+        {
+            printf("U MODE\n");
+            // Operating in the User (Minet) Stack
+            // We don't have to build this one for the project?
+            return 0;	// Operations were successful
+        }
+        else
+        {
+            return -1;		// There was an issue encountered
+        }
     }
+
+
 }
