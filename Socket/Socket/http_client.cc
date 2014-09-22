59c99658fdfc6c2a78cb8c1eecdb3cdc4eb35921
qrtrmstr12 <QM22may()josh.miner12@gmail.com>
2014-09-21 21:40:53 -0400

    started working on moving everything back
    Trying to make it match back to what he had as a skeleton and then
    finish client.

--- Socket/Socket/http_client.cc
+++ Socket/Socket/http_client.cc
@@ -1,6 +1,9 @@
-/* UNCOMMENT FOR MINET
- * #include "minet_socket.h"
- */
+/*
+	Joshua Miner	- jmm299
+	Ethan Welsh		- 
+	CS1652 Fall 2014
+	Project 1- http_client.cc
+*/
 
 #include <stdio.h>
 #include <string.h>
@@ -11,7 +14,7 @@
 #include <netdb.h>
 #include <assert.h>
 #include <unistd.h>
-
+#include <iostream>
 
 #define BUFSIZE 1024
 
@@ -22,7 +25,7 @@ int main(int argc, char * argv[])
     int server_port    = -1;
     char * server_path = NULL;
     char * req         = NULL;
-
+	bool ok            = false;	// Used to make sure that everything is good
 
     int socketID;
     int len;
@@ -30,8 +33,15 @@ int main(int argc, char * argv[])
     char buf[BUFSIZE];
     struct hostent *hp;
     struct sockaddr_in saddr;
-
-    if (argc != 5)
+	fd_set fds;
+	
+    string response = "";	// Hold response 
+	string header = "";	// Hold the header for processing
+	string status;		// Hold the expected status code
+	string::size_type position;
+	
+	// Check that all arguments were provided.
+	if (argc != 5)
     {
         fprintf(stderr, "usage: http_client k|u server port path\n");
         exit(-1);
@@ -43,6 +53,15 @@ int main(int argc, char * argv[])
 
     req = (char *)malloc(strlen("GET  HTTP/1.0\r\n\r\n")+strlen(server_path)+1);
 
+	if (server_port<= 0 || server_port> 65535) 
+	{
+		// If the port number is not valid
+    	fprintf(stderr, "Port number is invalid. Aborting.\n");
+		free(req);
+		return -1;
+    }
+	
+	// For initializing minet kernel
     if (toupper(*(argv[1])) == 'K')
     {
 
@@ -54,23 +73,27 @@ int main(int argc, char * argv[])
     else
     {
         fprintf(stderr, "First argument must be k or u\n");
-        exit(-1);
+        free(req);
+		exit(-1);
     }
 
 
     if ((socketID = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
     { //error processing;
         printf("Failed to establish socket.\n");
-        return -1;
+		free(req);
+		return -1;
     }
 
 
     if ((hp = gethostbyname(server_name)) == NULL)
     {  //error processing;
         printf("DNS could not locate the page you asked for.\n");
-        return -1;
+        free(req);
+		return -1;
     }
 
+	// Set the address
     saddr.sin_family = AF_INET;
     memcpy(&saddr.sin_addr.s_addr, hp->h_addr, hp->h_length);  /* TODO CHANGED LENGTH TO H_LENGTH*/
     saddr.sin_port = htons(server_port);
